d1a42c9d7e2a511814e8ce12d0d9f47bced06be6
Alexander-Govaers <12401919@student.pxl.be>
2025-01-22 22:10:58 +0100

    done

---
+++ WpfApp_talstelsel/MainWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp_talstelsel
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+    
+
+        private void calcButton_Click(object sender, RoutedEventArgs e)
+        {
+            int userInput = Convert.ToInt32(hexaTextBox.Text,16);
+            decimalTextBox.Text = $"{userInput:N}";
+        }
+
+        private void retryButton_Click(object sender, RoutedEventArgs e)
+        {
+            hexaTextBox.Clear();
+            decimalTextBox.Clear();
+        }
+
+        private void closeButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void hexaTextBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key >= Key.A && e.Key <= Key.F || e.Key == Key.Back)
+            {
+                e.Handled = false;
+                return;
+            }
+            if (e.Key >= Key.D0 && e.Key <= Key.D9 || e.Key >= Key.NumPad0 && e.Key <= Key.NumPad9)
+            {
+                e.Handled = false;
+                return;
+            }
+            else e.Handled = true;
+        }
+    }
+}
